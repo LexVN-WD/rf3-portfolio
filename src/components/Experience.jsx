27157e79b0fa177ca3a538c909efbccbbb5efa8a
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-06-30 15:38:52 -0600

    Add contact shadows to model

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { Environment, OrbitControls, Sky } from "@react-three/drei";
+import { Environment, OrbitControls, Sky, ContactShadows } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useControls } from "leva";
 
@@ -17,6 +17,7 @@ export const Experience = () => {
       <Sky />
       <Environment preset="sunset" />
       <group position-y={-1}>
+        <ContactShadows opacity={0.55} scale={10} blur={1} far={10} resolution={256} color="#000000" />
         <Avatar animation={animation}/>
         {animation === "Typing" && (
           <mesh scale={[0.8, 0.5, 0.8]} position-y={0.20}>
@@ -24,11 +25,9 @@ export const Experience = () => {
             <meshStandardMaterial color="white" />
           </mesh>
         )}
-        <mesh
-        receiveShadow
-        scale={5}
-        rotation-x={-Math.PI * 0.5}>
+        <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
           <planeGeometry />
+          <meshStandardMaterial color="white" />
         </mesh>
       </group>
     </>
