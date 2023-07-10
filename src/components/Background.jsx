2e86243e8a4de0b3027abcdeea73ab4f32a0c826
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-07-10 11:54:46 -0600

    Add background colors per page

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,10 +1,38 @@
-import {Sphere} from '@react-three/drei'
-import { useRef } from 'react'
+import {Sphere, useScroll} from '@react-three/drei'
+import { useFrame } from '@react-three/fiber';
+import { gsap } from 'gsap';
+import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 
 export default function Background() {
 
   const material = useRef();
+  const color = useRef({
+    color: "#14053b",
+  })
+
+  const data = useScroll();
+
+  const tl = useRef();
+
+  useFrame(() => {
+    tl.current.progress(data.scroll.current);
+    material.current.color = new THREE.Color(color.current.color);
+  })
+
+  useEffect(() => {
+    tl.current = gsap.timeline();
+    tl.current.to(color.current, {
+      color: "#14053b",
+    });
+    tl.current.to(color.current, {
+      color: "#3a1a8a",
+    });
+    tl.current.to(color.current, {
+      color: "#3a0da8",
+    });
+
+  }, []);
 
   return (
     <group>
