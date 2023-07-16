3565ceae951279b6a21c7ee82d60e31bd2144892
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-07-16 17:40:01 -0600

    Increase max height of About section

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -76,7 +76,7 @@ const AboutSection = (props) => {
 
   return (
      <Section mobileTop >
-      <div className="flex flex-col max-h-[50%] mb-2 md:h-full w-auto bg-gray-500 rounded-lg bg-opacity-40 md:bg-opacity-0 items-center">
+      <div className="flex flex-col max-h-[70%] mb-2 md:max-h-[60%] w-auto bg-gray-500 rounded-lg bg-opacity-40 md:bg-opacity-0 items-center">
         <motion.h1 
           className="text-2xl md:text-7xl font-extrabold leading-snug text-white mt-4 md:mt-6 md:pt-6 text-center"
           initial={{ opacity: 0, y: 25 }}
